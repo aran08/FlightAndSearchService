50c235a78aa42973e8ffd2e18302aed3691175c2
Aran Choudhary <aranjaish2308@gmail.com>
2023-07-10 03:11:30 +0530

    Added uique constraints & started cityrepository

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const CityRepository = require ('./repository/City-repository')
+const { PORT } = require("./config/serverConfig");
+
+const setupAndStartServer = async () => {
+  // create the express object
+  const app = express();
+
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+
+  app.listen(PORT, async () => {
+    console.log(`Server started at ${PORT}`);
+    
+    const repo = new CityRepository();
+    repo.createCity({name : "New delhi"});
+  });
+};
+
+setupAndStartServer();
