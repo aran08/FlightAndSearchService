187ee54bbf7209ef31d01c2a42347cacd115a679
Aran Choudhary <aranjaish2308@gmail.com>
2023-07-10 23:21:56 +0530

    Completed city Repository

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -4,9 +4,12 @@ class CityRepository {
 
     async createCity({ name }) {
         try {
-            const city = await City.create({ name });
+            const city = await City.create({ 
+                name
+             });
             return city;
         } catch (error) {
+            console.log("Something went wrong in the repository layer");
             throw {error};
         }
     }
@@ -19,8 +22,34 @@ class CityRepository {
                 }
             });
         } catch (error) {
+            console.log("Something went wrong in the repository layer");
             throw {error};
         }
+    }
+
+    async updateCity( cityId, name) {
+        try {
+            const city = await City.update( data, {
+                where: {
+                    Id :cityId
+                }
+            });
+            return city;
+        } catch (error) {
+            console.log("Something went wrong in the repository layer");
+            throw {error};
+        }
+
+    }
+
+    async getCity( cityId ) {
+        try {
+            const city = await City.findByPK( cityId );
+        } catch (error) {
+            console.log("Something went wrong in the repository layer");
+            throw {error};
+        }
+
     }
 }
 
